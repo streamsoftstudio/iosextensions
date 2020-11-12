be59b734e8ff6c51eaf13d2eb131c72b7970cb50
Andrija Milovanovic <andrija@streamsoft.rs>
2020-11-13 00:42:42 +0100

    Adding test target to scheme

---
+++ iOSExtensionsTests/iOSExtensionsTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOSExtensionsTests.swift
+//  iOSExtensionsTests
+//
+//  Created by Andrija Milovanovic on 11/13/20.
+//
+
+import XCTest
+
+class iOSExtensionsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
