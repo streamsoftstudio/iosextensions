f11f4e8a5c0fd86973fbcae9ec1cd5edba9f9f63
Dusan Juranovic <dusan.juranovic@gmail.com>
2021-08-10 13:46:00 +0200

    Additional UIKit extensions

---
+++ Sources/IOSExtensions/UIKit/calayer.swift
@@ -0,0 +1,31 @@
+//
+//  calayer.swift
+//  
+//
+//  Created by Dusan Juranovic on 10.8.21..
+//
+
+import UIKit
+
+extension CALayer {
+	func addShadow(_ offset: CGSize = CGSize(width: 0, height: 3.0), color: CGColor = UIColor.black.cgColor, opacity: Float = 0.2, radius: CGFloat = 2, rasterize: Bool = true) {
+		self.masksToBounds = false
+		self.shadowRadius = radius
+		self.shadowColor = color
+		self.shadowOpacity = opacity
+		self.shadowOffset = offset
+		
+		if rasterize {
+			self.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: radius).cgPath
+			self.shouldRasterize = true
+			self.rasterizationScale = UIScreen.main.scale
+		}
+	}
+	
+	func addCorners(_ corners:UIRectCorner, radius: CGFloat) {
+		let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+		let mask = CAShapeLayer()
+		mask.path = path.cgPath
+		self.mask = mask
+	}
+}
---
+++ Sources/IOSExtensions/UIKit/uibutton.swift
@@ -0,0 +1,26 @@
+//
+//  uibutton.swift
+//  
+//
+//  Created by Dusan Juranovic on 10.8.21..
+//
+
+import UIKit
+
+extension UIButton {
+	/// Sets background color for a specific UIButton  state
+	/// - Parameters:
+	///   - color: Background color
+	///   - forState: UIButton state
+	func setBackgroundColor(color: UIColor, forState: UIControl.State) {
+		self.clipsToBounds = true  // add this to maintain corner radius
+		UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+		if let context = UIGraphicsGetCurrentContext() {
+			context.setFillColor(color.cgColor)
+			context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+			let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+			UIGraphicsEndImageContext()
+			self.setBackgroundImage(colorImage, for: forState)
+		}
+	}
+}
