2fb050d6aa12ebd11600a5f2c3ffad0f351b83a8
Dusan Juranovic <dusan.juranovic@gmail.com>
2021-08-28 11:28:09 +0200

    Added comments

--- Sources/IOSExtensions/UIKit/uilabel.swift
+++ Sources/IOSExtensions/UIKit/uilabel.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 public extension UILabel {
+	/// A simple ellipsis animation extension on UILabel.
+	/// - Parameters:
+	///   - string: `UILabel` text on which ellipsis animation will be added.
+	///   - timeInterval: `timeInterval` between each ellipsis element drawing. Defaults to 0.5.
+	///   - ellipsisSymbol: `String` symbol to use for ellipsis elements. Defaults to a `dot` (`.`).
+	///   - ellipsisCount: number of ellipsis elements to animate. Defaults to 3.
 	func animateEllipsis(_ string: String, timeInterval: TimeInterval = 0.5, ellipsisSymbol: String = ".", ellipsisCount: Int = 3) {
 		self.text = string
 		var index = 1
